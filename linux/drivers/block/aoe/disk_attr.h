0936c6b9e2bfcb223ecc950e81d208bf925a0774
Ed Cashin <ed.cashin@acm.org>
2015-04-02 21:05:13 -0400

    Add a .gitignore file

--- linux/drivers/block/aoe/disk_attr.h
+++ linux/drivers/block/aoe/disk_attr.h
@@ -1,1 +1,8 @@
-/* struct disk_attribute is defined in kernel headers */
+/* This struct is not defined in kernels 2.6.10 and earlier.
+ * It is defined here for compatibility with those kernels.
+ */
+
+struct disk_attribute {
+	struct attribute attr;
+	ssize_t (*show)(struct gendisk *, char *);
+};
