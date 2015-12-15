21cac78dce047995bf44275b70943ce14a8f4139
Ed Cashin <ed.cashin@acm.org>
2015-12-14 21:06:05 -0500

    Correct type bvec_iter, completing the work of fd0d590c7

--- conf/41-kv.sh
+++ conf/41-kv.sh
@@ -3,6 +3,6 @@ echo 'obj-$(CONFIG_ATA_OVER_ETH) += '"$TESTNO"'.o' > conf/Makefile
 $make_cmd > conf/$TESTNO.log 2>&1
 
 status="$?"
-test "`egrep -i '(err|warn).*undefined type .struct biovec_iter' conf/$TESTNO.log`" && { echo old; exit 0; }
+test "`egrep -i '(err|warn).*undefined type .struct bvec_iter' conf/$TESTNO.log`" && { echo old; exit 0; }
 
 echo new
