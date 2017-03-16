7184a9abafefd6faeb0e302ab430b95ec13f9d93
Ed Cashin <ed.cashin@acm.org>
2017-03-15 20:52:05 -0400

    Recommending in-kernel aoe driver usage

--- linux/drivers/block/aoe/aoe.h
+++ linux/drivers/block/aoe/aoe.h
@@ -1,5 +1,5 @@
 /* Copyright (c) 2013 Coraid, Inc.  See COPYING for GPL terms. */
-#define VERSION "87"
+#define VERSION "87moth"
 #define AOE_MAJOR 152
 #define DEVICE_NAME "aoe"
 #define DEVICE_SUBDIR "etherd"
